bb97bc92eebfd8ee9a14025c3b4b3ade0bb1c9db
VISHAL SAINI <112400743+sainivishal70556@users.noreply.github.com>
2024-03-01 23:09:09 +0530

    Update proxey.sol

--- proxey.sol/proxey.sol
+++ proxey.sol/proxey.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract LogicContract {
+    uint public value;
+
+    function updateValue(uint _newValue) external {
+        value = _newValue;
+    }
+}
+
+contract ProxyContract {
+    address public logicContract;
+
+    constructor(address _logicContract) {
+        logicContract = _logicContract;
+    }
+
+    function updateValue(uint _newValue) external {
+        (bool success, ) = logicContract.delegatecall(abi.encodeWithSignature("updateValue(uint256)", _newValue));
+        require(success, "Update failed");
+    }
+}
 
